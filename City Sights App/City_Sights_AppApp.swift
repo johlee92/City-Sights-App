d8d7be85e2f10e7dced0210238f35bfd488f1cc7
John <johnathanlee72@hotmail.com>
2022-04-25 21:11:59 -0700

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Johnathan Lee on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
